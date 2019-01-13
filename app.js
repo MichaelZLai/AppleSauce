478f125f2fd9eaa09523bb9f6d0b76af8f792e00
Michael Lai <mike24567@gmail.com>
2019-01-13 01:21:45 -0800

    commenting out

--- app.js
+++ app.js
@@ -4,45 +4,45 @@ const request = require('request');
 var express = require('express');
 var app = express();
 
-
-// Replace <Subscription Key> with your valid subscription key.
-const subscriptionKey = 'dc565dfb55344704879376ec526d78a3';
-
-// You must use the same location in your REST call as you used to get your
-// subscription keys. For example, if you got your subscription keys from
-// westus, replace "westcentralus" in the URL below with "westus".
-const uriBase =
-    'https://westus.api.cognitive.microsoft.com/vision/v2.0/analyze';
-
-const imageUrl =
-    'http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
-
-// Request parameters.
-const params = {
-    'visualFeatures': 'Categories,Description,Color',
-    'details': '',
-    'language': 'en'
-};
-
-const options = {
-    uri: uriBase,
-    qs: params,
-    body: '{"url": ' + '"' + imageUrl + '"}',
-    headers: {
-        'Content-Type': 'application/json',
-        'Ocp-Apim-Subscription-Key' : subscriptionKey
-    }
-};
-
-request.post(options, (error, response, body) => {
-  if (error) {
-    console.log('Error: ', error);
-    return;
-  }
-  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
-  console.log('JSON Response\n');
-  console.log(jsonResponse);
-});
+//
+// // Replace <Subscription Key> with your valid subscription key.
+// const subscriptionKey = 'dc565dfb55344704879376ec526d78a3';
+//
+// // You must use the same location in your REST call as you used to get your
+// // subscription keys. For example, if you got your subscription keys from
+// // westus, replace "westcentralus" in the URL below with "westus".
+// const uriBase =
+//     'https://westus.api.cognitive.microsoft.com/vision/v2.0/analyze';
+//
+// const imageUrl =
+//     'http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
+//
+// // Request parameters.
+// const params = {
+//     'visualFeatures': 'Categories,Description,Color',
+//     'details': '',
+//     'language': 'en'
+// };
+//
+// const options = {
+//     uri: uriBase,
+//     qs: params,
+//     body: '{"url": ' + '"' + imageUrl + '"}',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Ocp-Apim-Subscription-Key' : subscriptionKey
+//     }
+// };
+//
+// request.post(options, (error, response, body) => {
+//   if (error) {
+//     console.log('Error: ', error);
+//     return;
+//   }
+//   let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+//   console.log('JSON Response\n');
+//   console.log(jsonResponse);
+// });
 
 
 app.get('/', function (req, res) {
