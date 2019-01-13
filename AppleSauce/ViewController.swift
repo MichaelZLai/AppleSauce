1251378678e9a406e02ea6d12a8958b05e9ee784
Rahul Chandra <rahulc7@uw.edu>
2019-01-12 19:03:20 -0800

    adding swift

---
+++ AppleSauce/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  AppleSauce
+//
+//  Created by Rahul Chandra on 1/12/19.
+//  Copyright © 2019 Rahul Chandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    let imagePicker = UIImagePickerController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = image
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
+        else{
+            print("oopsie getting image")
+        }
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
