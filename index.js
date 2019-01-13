41f03706a7191008540916b41a1c2e5a539c9e5d
Michael Lai <mike24567@gmail.com>
2019-01-12 23:31:57 -0800

    adding upload feature

--- index.js
+++ index.js
@@ -48,3 +48,27 @@ request.post(options, (error, response, body) => {
 app.get('/', function (req, res) {
   res.send(jsonResponse);
 });
+
+app.post('/upload', function (req, res) {
+
+    var form = new formidable.IncomingForm();
+    //console.log(form);
+    //console.log(req._readableState);
+    //console.log(req.body);
+    form.parse(req, function(err, fields, files) {
+        res.end('success');
+    });
+    form.on('end', function(fields, files) {
+         var temp_path = this.openedFiles[0].path;
+         //console.log(temp_path);
+         var file_name = this.openedFiles[0].name;
+         var new_location = path.dirname(require.main.filename) + "/uploads/fullsize/";
+         fs.copy(temp_path, new_location + file_name, function(err) {
+            if (err) {
+                 console.error(err);
+            } else {
+                   console.log("success!")
+            }
+        });
+    });
+});
